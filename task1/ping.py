1f4a1494b9f548ac7272828fb4e68184d33e4a53
buklinfur <buklinfur@outlook.com>
2025-02-11 11:13:56 +0700

    update task1

--- task1/ping.py
+++ task1/ping.py
@@ -5,6 +5,18 @@ from pythonping import ping
 result_path = "ping_results.csv"
 
 
+addresses = ["google.com",
+             "tetr.io",
+             "github.com",
+             "en.wikipedia.com",
+             "nsu.ru",
+             "fresh.nsuts.ru",
+             "orel-region.ru",
+             "president.gov.by",
+             "vk.com",
+             "mail.google.com"]
+
+
 def save_to_csv(host, min_ms, avg_ms, max_ms):
     df = pd.DataFrame([[host, min_ms, avg_ms, max_ms]])
     df.to_csv(result_path, mode='a', index=False, header=False)
@@ -17,18 +29,6 @@ def get_rtt(addresses_arr):
         response = ping(address)
         save_to_csv(address, response.rtt_min_ms, response.rtt_avg_ms,
                     response.rtt_max_ms)
-
-
-addresses = ["google.com",
-             "tetr.io",
-             "github.com",
-             "en.wikipedia.com",
-             "nsu.ru",
-             "fresh.nsuts.ru",
-             "orel-region.ru",
-             "president.gov.by",
-             "vk.com",
-             "mail.google.com"]
 
 
 get_rtt(addresses)
